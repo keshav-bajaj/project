05b67e016d0bf54cb0bc899645bf9ed19fa4bc41
keshav-bajaj <bajajkeshav111@gmail.com>
2020-12-01 11:25:07 +0530

    update files

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 > Color Flipper
  * Changes color on click.
- * A simple Javascript Project .
+ * A simple Javascript Project.
  * See the project in action **[here](https://keshav-bajaj.github.io/projects/colorflipper/)**.
  
  
@@ -15,7 +15,10 @@
 
 > Drawing Tool
  * Let's you draw on screen with different colors and pen sizes.
- * Made with Javascript and HTML5 Canvas .
+ * Made with Javascript and HTML5 Canvas.
  * See the project in action **[here](https://keshav-bajaj.github.io/projects/drawingtool/)**.
  
- 
+> Find The Cat
+ * A Game in which you have to find cats on your screen.
+ * Made with Javascript, HTML5 and CSS.
+ * Play the game **[here](https://keshav-bajaj.github.io/projects/findthecat/)**.
--- findthecat/app.js
+++ findthecat/app.js
@@ -1,3 +1,4 @@
+const scorebox = document.getElementById("score");
 class cat {
   constructor() {
     var img = document.createElement("img");
@@ -11,20 +12,22 @@ class cat {
     img.style.transform = "rotate(" + Math.floor(Math.random() * 360) + "deg)";
     img.addEventListener("click", () => {
       img.classList.add("appear");
+      new cat();
       setTimeout(() => {
-        new cat();
         img.remove();
       }, 2500);
-      var score = document.getElementById("score").innerHTML;
+      var score = scorebox.innerHTML;
       score++;
-      document.getElementById("score").innerHTML = score;
+      scorebox.innerHTML = score;
     });
     document.body.appendChild(img);
   }
 }
-
 const startbtn = document.getElementById("start");
 const timer = document.getElementById("timer");
+const gamediv = document.getElementById("game-div");
+const endbox = document.getElementById("end-game");
+const startdiv = document.getElementById("start-div");
 function gametimer() {
   var seconds = 59;
   timer.innerHTML = 0 + ":" + seconds;
@@ -37,29 +40,54 @@ function gametimer() {
     timer.click();
   }, 1000);
 
-  setInterval(() => {
+  var endtimer = setInterval(() => {
     if (seconds <= 0) {
       clearInterval(clock);
+      clearInterval(endtimer);
+      endgame();
     }
   }, 1);
 }
 
 function game() {
-    document.getElementById("start-div").remove();
-    document.getElementById("game-div").style.display = "block";
-    gametimer();
-    new cat();
+  scorebox.innerHTML = 0;
+  startdiv.style.display = "none";
+  gamediv.style.display = "block";
+  endbox.style.display = "none";
+  gametimer();
+  new cat();
 }
 
-startbtn.addEventListener("click" , game);
-document.getElementById("start-again").addEventListener("click" , game);
+startbtn.addEventListener("click", game);
+document.getElementById("start-again").addEventListener("click", game);
 
 function endgame() {
-    var cats = document.querySelectorAll("#cat");
-    for (i=0;i<cats.length;i++){
-        cats[i].remove();
-    }
-    document.getElementById("game-div").style.display = "none";
-    document.getElementById("end-game").style.display = "block";
+  removecats();
+  gamediv.style.display = "none";
+  endbox.style.display = "block";
+  document.getElementById("current-score").innerHTML = scorebox.innerHTML;
+  setscore();
+  document.getElementById("high-score").innerHTML = localStorage.getItem(
+    "Hscore"
+  );
 }
 
+function setscore() {
+  var score = scorebox.innerHTML;
+  if (localStorage.getItem("Hscore") === null) {
+    localStorage.setItem("Hscore", score);
+  } else {
+    if (localStorage.getItem("Hscore") > score) {
+      return false;
+    } else {
+      localStorage.setItem("Hscore", score);
+    }
+  }
+}
+
+function removecats() {
+  var cats = document.querySelectorAll("img");
+  for(i=0;i<cats.length;i++){
+    cats[i].remove();
+  }
+}
